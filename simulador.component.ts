3444f5d786764f09831da4e4a59f026845b6bdb6
arleyop <ycastellanosc1@ucentral.edu.co>
2025-08-25 12:09:41 -0500

    Update simulador.component.ts

--- simulador.component.ts
+++ simulador.component.ts
@@ -1,22 +1,4 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-export interface DerivadoRequest {
-  tipoIdentificacion: string;
-  exposicion: number;
-  fechaOperacion: string; // YYYY-MM-DD
-  fechaCarga: string;     // YYYY-MM-DD
-}
-
-@Injectable({ providedIn: 'root' })
-export class FichaDerivadoService {
-  private baseUrl = '/api/derivados';
-
-  constructor(private http: HttpClient) {}
-
-  // Nuevo método: recibe directamente las filas de la tabla
-  guardarDerivados(filas: DerivadoRequest[]): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/guardar`, filas);
-  }
-}
+  private String tipoIdentificacion;
+    private Double exposicion;
+    private LocalDate fechaOperacion;
+    private LocalDate fechaCarga;
