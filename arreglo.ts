6335f74e44987e5eef666fd2a9f932916af54060
arleyop <ycastellanosc1@ucentral.edu.co>
2025-08-26 15:29:49 -0500

    Update arreglo.ts

--- arreglo.ts
+++ arreglo.ts
@@ -1,120 +1,83 @@
-<div class="container">
-  <div class="row">
-    <div class="col-8 mx-auto">
-      <form [formGroup]="parametriaMonedaFormGroup">
-        <div class="container py-5">
-          <div class="row align-items-center">
-            <div class="col">
-              <h1>Moneda</h1>
-            </div>
-            <div class="col-auto">
-              <div class="row">
-                <button
-                  mat-stroked-button
-                  color="primary"
-                  (click)="
-                    this.parametriaMonedaFormGroup.value.idMoneda
-                      ? (this.parametriaMonedaFormGroup.value.idMoneda = null)
-                      : setNewParametriaMoneda()
-                  "
-                >
-                  <mat-icon
-                    [svgIcon]="
-                      this.parametriaMonedaFormGroup.value.idMoneda
-                        ? 'close'
-                        : 'add'
-                    "
-                  ></mat-icon>
-                  {{
-                    this.parametriaMonedaFormGroup.value.idMoneda
-                      ? "Cancelar"
-                      : "Crear nueva Moneda"
-                  }}
-                </button>
-              </div>
-            </div>
-          </div>
-          <!-- Campos del fomulario -->
-          <div
-            [@openClose]="
-              this.parametriaMonedaFormGroup.value.idMoneda ? 'open' : 'closed'
-            "
-            class="row justify-content-center overflow-hidden"
-          >
-            <div class="col-8 mb-5">
-              <div class="row">
-                <div class="col">
-                  <h2>
-                    {{
-                      this.parametriaMonedaFormGroup.value.idMoneda !=
-                      -1
-                        ? "Actualizar Tipo Moneda " 
-                        : "Crear Tipo Moneda"
-                    }}
-                  </h2>
-                </div>
-              </div>
-              <div class="row row-cols-2 gx-6">
-                <div class="col d-flex justify-content-center">
-                  <mat-form-field>
-                    <mat-label>Pesos colombianos (%) :</mat-label>
-                    <input type="number" matInput formControlName="pesosColombianos" />
-                  </mat-form-field>
-                </div>
-                <div class="col d-flex justify-content-center">
-                  <mat-form-field>
-                    <mat-label>USD (%):</mat-label>
-                    <input type="number" matInput formControlName="usd" />
-                  </mat-form-field>
-                </div>
-                <div class="col d-flex justify-content-center">
-                  <mat-form-field>
-                    <mat-label>Otras monedas extranjeras (%):</mat-label>
-                    <input type="number" matInput formControlName="otrasMonedasExt" />
-                  </mat-form-field>
-                </div>
-              </div>
-              <div class="row justify-content-center">
-                <div class="col-6">
-                  <div class="row">
-                    <div class="row justify-content-center">
-                      <div class="row">
-                        <app-request-button
-                          (click)="onSaveButtonClicked()"
-                          [isLoading]="isFormLoading"
-                          [disabled]="!getSaveButtonEnabled()"
-                          >{{
-                            this.parametriaMonedaFormGroup.value.idMoneda != -1
-                              ? "Actualizar moneda"
-                              : "Crear moneda"
-                          }}</app-request-button
-                        >
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row mt-5">
-            <h2 color="primary">Versión Moneda</h2>
-          </div>
-          <div class="row">
-            <app-shared-table
-              [dataSource]="tableDataSource"
-              [columnConfig]="columnConfig"
-              [colmunId]="colmunId"
-              [enableSearch]="true"
-              [searchValue]="searchValue"
-              [searchLabel]="'Usuarios'"
-              [searchColumn]="'Usuario'"
-              [searchLabel]="'Buscar por usuario'"
-              [stateLabel]="tableStateLabel"
-              [isProcessing]="isTableLoading"
-            ></app-shared-table>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
+@Id
+    @Column(name = "fec_periodo", nullable = false)
+  	private LocalDate fecPeriodo;
+
+    @Id
+    @Column(name = "contrato", length = 20, nullable = false)
+    private String contrato;
+
+    @Column(name = "monto", precision = 17, scale = 4)
+    private BigDecimal monto;
+    
+    @OneToMany (mappedBy = "derivados")
+    private Set<ReClientesObligacion> reClienteObligacion;
+    
+    @OneToMany(mappedBy = "derivados")
+   	private Set<RePrestamosGarantias> prestamosGarantias ;
+    
+    @Column(name = "FECHA_VCTO")
+    private LocalDate fecha_vcto;
+
+
+
+
+
+package com.santander.bnc.bsn021.bncbsn021gemotorbackend.service.impl;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.santander.bnc.bsn021.bncbsn021gemotorbackend.dto.DerivadoRequestDTO;
+import com.santander.bnc.bsn021.bncbsn021gemotorbackend.model.motor.Derivados;
+import com.santander.bnc.bsn021.bncbsn021gemotorbackend.repository.motor.DerivadosRepository;
+import com.santander.bnc.bsn021.bncbsn021gemotorbackend.service.DerivadoService;
+import com.santander.bnc.bsn021.bncbsn021gemotorbackend.service.MonitoreoService;
+import com.santander.bnc.bsn021.bncbsn021gemotorbackend.utils.ConstantsUtil;
+
+@Service
+public class DerivadoServiceImpl implements DerivadoService {
+
+	@Autowired
+	private DerivadosRepository derivadoRepository;
+	
+	@Autowired
+	private MonitoreoService monitoreoService;
+	
+
+	@Override
+	public List<Derivados> guardarDerivados(List<DerivadoRequestDTO> requests) throws Exception {
+		// 1. Limpiar la tabla completa
+		try {
+		derivadoRepository.deleteAll();
+		} catch (Exception e) {
+			throw new Exception("Error al borrar Derivados", e);
+		}
+		// 2. Mapear requests a entidades
+		List<Derivados> derivados = new ArrayList<>();
+
+		for (DerivadoRequestDTO data : requests) {
+			Derivados derivado = new Derivados();
+			derivado.setMonto(BigDecimal.valueOf(data.getExposicion()));
+			derivado.setContrato(data.getTipoIdentificacion());
+			derivado.setFecha_vcto(data.getFechaOperacion());
+			derivado.setFecPeriodo(data.getFechaOperacion());
+			derivados.add(derivado);
+		}
+		try {
+		
+		monitoreoService.registrarAccionMonitoreo(ConstantsUtil.PROCESOS_CARGA_ACCION_GUARDAR, ConstantsUtil.MODULO_PROCESOS_CARGA_DERIVADOS);
+		
+		// 3. Guardar nuevos
+		return derivadoRepository.saveAll(derivados);
+		
+		} catch (Exception e) {
+			throw new Exception("Error al Guardar Derivados", e);
+		}
+		
+	}
+
+}
