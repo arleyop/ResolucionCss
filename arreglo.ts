4b2a045e99b20f4e898bc5f1a9b661b8bd418552
arleyop <ycastellanosc1@ucentral.edu.co>
2025-09-02 11:49:22 -0500

    Update arreglo.ts

--- arreglo.ts
+++ arreglo.ts
@@ -1,1 +1,23 @@
-respuesta de tipo tasa[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object]
+export interface GemTasaCambio {
+  cdDiviss: string;   // Código de divisa
+  cambFix: number;    // Valor de la tasa
+  fhCambio: string;   // Fecha de cambio (ISO string)
+}
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GemTasaCambio } from '../models/gem-tasa-cambio.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasaCambioService {
+  private apiUrl = '/api/tasas-cambio';
+
+  constructor(private http: HttpClient) {}
+
+  getcurrencyTypeCombonBoxData(): Observable<{ status: string, data: GemTasaCambio[] }> {
+    return this.http.get<{ status: string, data: GemTasaCambio[] }>(`${this.apiUrl}/tipos-moneda`);
+  }
+}
